92b0ba269f2c14656fbbb2414aae972214c5c7cd
HamnaYasin34 <130100572+HamnaYasin34@users.noreply.github.com>
2023-05-08 10:12:12 -0700

    Add files via upload

---
+++ script.js
@@ -0,0 +1,21 @@
+// #3 VARIABLES FOR NUMBERS
+
+// price = 450;
+// Discount = 50;
+// document.write(price - Discount);
+
+console.log("#3 VARIABLES FOR NUMBERS");
+
+var age = "I am 17 Years old";
+alert(age);
+
+var visitor = 16;
+alert(" You have visited this site " + 14 + " times ");
+
+var birthyear = 1;
+document.write('What is Your birthyear ? <br> ' + ' <br> My Birth Year is 2005 <br> <br> ');
+
+var name = " Hamna Yasin ";
+var quantity = "6";
+var product = "Hoodies";
+document.write(name + " Ordered " + quantity + " " + product + " On XYZ Cloth Store ");
